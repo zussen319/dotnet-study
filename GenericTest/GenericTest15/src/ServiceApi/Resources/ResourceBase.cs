c9fab7b7bafec9f22ef0fdb6baf5c6228ba9d4f6
zussen319 <zussen319@example.com>
2026-05-17 10:04:06 +0900

    GenericTest15: リソースクラス：エラーメッセージ修正

--- GenericTest/GenericTest15/src/ServiceApi/Resources/ResourceBase.cs
+++ GenericTest/GenericTest15/src/ServiceApi/Resources/ResourceBase.cs
@@ -10,9 +10,6 @@ internal abstract class ResourceBase(Type resourceType)
     private readonly ResourceManager _resourceManager = new(resourceType);
 
     protected string GetString(string key)
-    {
-        // 意図を明確にするため、例外メッセージのクラス名取得を補正
-        return _resourceManager.GetString(key)
-            ?? throw new KeyNotFoundException($"リソースキー'{key}'が'{GetType().Name}'に見つかりません");
-    }
+        => _resourceManager.GetString(key) ??
+            throw new KeyNotFoundException($"リソースキー'{key}'が'{resourceType.Name}'に見つかりません");
 }
